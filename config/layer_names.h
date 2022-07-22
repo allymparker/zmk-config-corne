14148f8dcdd5ef327a15176edf07907b030b1bcd
Ally Parker <ally@theparkers.xyz>
2022-07-22 16:34:27 +0100

    Revert "Win first"
    This reverts commit ee6e0028255e844c6882f199b87ba5fe9f95f485.

--- config/layer_names.h
+++ config/layer_names.h
@@ -1,9 +1,9 @@
-#define WIN_BASE 0
-#define WIN_SYM  1
-#define WIN_NAV  2
-#define BASE     3
-#define SYM      4
-#define NAV      5
+#define BASE     0
+#define SYM      1
+#define NAV      2
+#define WIN_BASE 3
+#define WIN_SYM  4
+#define WIN_NAV  5
 #define NUM      6
 #define FUN      7
 #define MEDIA    8
