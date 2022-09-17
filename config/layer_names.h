c506e68e1f275b3ae6c42cbac660598130bfccee
Ally Parker <ally@theparkers.xyz>
2022-09-17 13:23:36 +0100

    Remove Windows

--- config/layer_names.h
+++ config/layer_names.h
@@ -1,9 +1,6 @@
 #define BASE     0
 #define SYM      1
 #define NAV      2
-#define WIN_BASE 3
-#define WIN_SYM  4
-#define WIN_NAV  5
-#define NUM      6
-#define FUN      7
-#define MEDIA    8
+#define NUM      3
+#define FUN      4
+#define MEDIA    5
